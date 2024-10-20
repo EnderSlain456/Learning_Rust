0ebc0e8f66fbb90c750513d5f921ed645d56377b
EnderSlain456 <74849071+EnderSlain456@users.noreply.github.com>
2024-10-20 14:55:13 +0100

    added more variables

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,23 @@
-fn main() {
-    let x = 5;
+fn main () {
+
+    let mut x = 5;
     let y = 10;
     let z = 15;
 
-    println!("{} {} {}", x, y, z)
+
+    println!("Initial values: {} {} {}", x,  y, z);
+
+
+    x = 20;
+    println!("Updated x: {}", x);
+
+    const MAX_POINTS: i32 = 100;
+    println!("Constant value: {}", MAX_POINTS);
+
+    let is_active: bool = true;
+    let letter: char = 'R';
+    let pi: f64 = 3.14159;
+
+
+    println!("Bool {}, Char: {}, Float: {}", is_active, letter, pi);
 }
